91cda66b1f62036d5b5090af59d4433af6036e01
glebm <glebmuravlev0511@gmail.com>
2025-04-04 16:41:12 +0300

    optimized code and add random support in code library

--- ok-face-mixer-core/src/lib.rs
+++ ok-face-mixer-core/src/lib.rs
@@ -1,6 +1,7 @@
-use std::{fmt::Display, str::FromStr};
+use strum_macros::{Display, EnumIter, EnumString};
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, EnumString, Display, EnumIter)]
+#[strum(serialize_all = "snake_case")]
 pub enum SmileType {
     Grin,
     Angry,
@@ -20,60 +21,6 @@ pub enum SmileType {
     Zany,
 }
 
-impl Display for SmileType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Self::Grin => "grin",
-                Self::Angry => "angry",
-                Self::Flush => "flush",
-                Self::He => "he",
-                Self::Mad => "mad",
-                Self::Plead => "plead",
-                Self::Sad => "sad",
-                Self::Sg => "sg",
-                Self::Shock => "shock",
-                Self::SlSmile => "sl_smile",
-                Self::Sleep => "sleep",
-                Self::Smiley => "smiley",
-                Self::Tong => "tong",
-                Self::Unamus => "unamus",
-                Self::Wink => "wink",
-                Self::Zany => "zany",
-            }
-        )
-    }
-}
-
-impl FromStr for SmileType {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.replace("/json/", "").replace(".json", "").as_str() {
-            "grin" => Ok(Self::Grin),
-            "angry" => Ok(Self::Angry),
-            "flush" => Ok(Self::Flush),
-            "he" => Ok(Self::He),
-            "mad" => Ok(Self::Mad),
-            "plead" => Ok(Self::Plead),
-            "sad" => Ok(Self::Sad),
-            "sg" => Ok(Self::Sg),
-            "shock" => Ok(Self::Shock),
-            "sl_smile" => Ok(Self::SlSmile),
-            "sleep" => Ok(Self::Sleep),
-            "smiley" => Ok(Self::Smiley),
-            "tong" => Ok(Self::Tong),
-            "unamus" => Ok(Self::Unamus),
-            "wink" => Ok(Self::Wink),
-            "zany" => Ok(Self::Zany),
-
-            _ => Err(()),
-        }
-    }
-}
-
 pub struct Smile {
     pub left: SmileType,
     pub right: SmileType,
@@ -89,6 +36,21 @@ impl Smile {
 impl Smile {
     pub fn api_query(&self) -> String {
         format!("?left={}&right={}", self.left, self.right)
+    }
+}
+
+#[cfg(feature = "rand")]
+impl Distribution<SmileType> for StandardUniform {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> SmileType {
+        use rand::{
+            Rng,
+            distr::{Distribution, StandardUniform},
+        };
+        use strum::IntoEnumIterator;
+
+        SmileType::iter()
+            .get(rng.random_range(..SmileType::iter().len()))
+            .unwrap()
     }
 }
 
